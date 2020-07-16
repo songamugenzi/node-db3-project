aeab4408676a271e30e7e45db62334fb9501d07d
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-07-15 20:29:12 -0400

    Met Multi-Table Queries project MVP

--- queries.sql
+++ queries.sql
@@ -1,27 +1,30 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
-select c.CategoryName, p.ProductName
-from [Product] as p
- join Category as c
- on p.CategoryId = c.Id
+SELECT p.ProductName, c.CategoryName
+FROM Product as p
+JOIN Category as c
+	on p.CategoryId = c.Id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-select shipper.CompanyName, [Order].Id, [Order].OrderDate 
-from [Order]
-join Shipper
-on [Order].ShipVia = shipper.Id
-where OrderDate < '2012-08-09'
+SELECT o.Id, s.CompanyName, o.OrderDate
+FROM [Order] as o
+JOIN Shipper as s
+	on o.ShipVia = s.Id
+WHERE o.OrderDate < "2012-08-09";
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
-select ProductName, count(OrderDetail.id)
-from Product
-join OrderDetail
-on Product.Id = OrderDetail.ProductId
-where OrderDetail.OrderId = 10251
+SELECT p.ProductName, o.Quantity
+FROM OrderDetail as o
+JOIN Product as p 
+    on p.id = o.ProductId
+WHERE o.OrderID = "10251"
+ORDER BY p.ProductName;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
-select [Order].id ,Customer.CompanyName, employee.LastName
-from [Order]
-join Employee, Customer
-where [Order].CustomerId = [Customer].Id AND [Order].EmployeeId = [Employee].Id
+SELECT o.id, c.CompanyName, e.LastName
+FROM [Order] as o
+JOIN Customer as c
+	on c.id = o.CustomerId
+JOIN Employee as e
+	on e.id = o.EmployeeId;
---
+++ schemes/scheme-model.js
@@ -0,0 +1,56 @@
+const db = require("../data/db-config.js");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+};
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first()
+    .then((scheme) => {
+      if (!scheme) {
+        return null;
+      } else {
+        return scheme;
+      }
+    });
+}
+
+function findSteps(id) {
+  return db("steps as c")
+    .join("schemes as s", "s.id", "c.scheme_id")
+    .select("s.scheme_name", "c.step_number", "c.instructions")
+    .where({ scheme_id: id })
+    .orderBy("c.step_number");
+}
+
+function add(schemeData) {
+  return db("schemes")
+    .insert(schemeData, "id")
+    .then(([id]) => {
+      return findById(id);
+    });
+}
+
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes)
+    .then(() => {
+      return findById(id);
+    });
+}
+
+function remove(id) {
+  return db("schemes").where({ id }).del();
+}
